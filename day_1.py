763aa6253379b3842b8b363baabab137e0694884
Zach Quinn <zacharytq@gmail.com>
2021-10-29 14:16:45 -0400

    solution for day 1.1

--- day_1.py
+++ day_1.py
@@ -1,5 +1,14 @@
+target_number = 2020
+
 with open('input_day_1.txt') as input:
     nums = []
-    while (line := int(input.readline().rstrip())):
-        nums.append(line)
+    while (line := input.readline().rstrip()):
+        nums.append(int(line))
+    nums.sort()
+    for i, number in enumerate(nums):
+        complementary_number = target_number - number
+        if complementary_number in nums[i+1:]:
+            product = complementary_number * number
+            print('Number 1: {} Number 2 {} Product: {}'.format(number, complementary_number, product))
+            break
 
