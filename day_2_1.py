7ca089e78b616328fb6488f19501b3b5dcb7df67
Zach Quinn <zacharytq@gmail.com>
2021-11-02 16:45:42 -0400

    solution for day 2.1

---
+++ day_2_1.py
@@ -0,0 +1,24 @@
+def parse_policy_line(line):
+    line_list = line.split(' ')
+    minimum = int(line_list[0].split('-')[0])
+    maximum = int(line_list[0].split('-')[1])
+    character = line_list[1][0]
+    password = line_list[2]
+    return dict([('min', minimum), ('max', maximum), ('char', character), ('password', password)])
+
+def policy_dict_list(policy_list):
+    dict_list = []
+    for i, policy in enumerate(policy_list):
+        dict_list.append(parse_policy_line(policy))
+    return dict_list
+
+def eval_policy(policy):
+    return policy['min'] <= policy['password'].count(policy['char']) <= policy['max']
+
+def count_passing(policy_list):
+    count = 0
+    for i, policy in enumerate(policy_list):
+        if eval_policy(policy):
+            count += 1
+    return count
+
